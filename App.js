00ec19965f1a354f0da3d0ab644062b2c5fa68fc
Emmanuel Cruz <emmanuel-cruz-dev@users.noreply.github.com>
2025-01-24 12:21:14 -0300

    primer commit

--- App.js
+++ App.js
@@ -1,11 +1,39 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { useEffect, useState } from "react";
+import {
+  StyleSheet,
+  Text,
+  View,
+  Image,
+  ScrollView,
+  SafeAreaView,
+} from "react-native";
+import { getProducts } from "./lib/metacritic";
 
 export default function App() {
+  const [products, setProducts] = useState([]);
+
+  useEffect(() => {
+    getProducts().then((products) => {
+      setProducts(products);
+    });
+  }, []);
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <StatusBar style="light" />
+      <SafeAreaView>
+        <ScrollView>
+          {products.map((item) => (
+            <View key={item.id} style={styles.card}>
+              <Image source={item.image} style={styles.image} />
+              <Text style={styles.title}>{item.title}</Text>
+              <Text style={styles.price}>${item.price}</Text>
+              <Text style={styles.description}>{item.description}</Text>
+            </View>
+          ))}
+        </ScrollView>
+      </SafeAreaView>
     </View>
   );
 }
@@ -13,8 +41,32 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#000",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  title: {
+    fontSize: 20,
+    fontWeight: "bold",
+    color: "#fff",
+    marginTop: 10,
+  },
+  price: {
+    fontSize: 18,
+    fontWeight: "bold",
+    color: "green",
+  },
+  description: {
+    fontSize: 16,
+    color: "#eee",
+  },
+  card: {
+    marginBottom: 42,
+  },
+  image: {
+    width: 120,
+    height: 150,
+    borderRadius: 10,
+    objectFit: "cover",
   },
 });
