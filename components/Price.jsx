a1115dc2257ec307dbc5f5fe6270406ba3b4ec2f
Emmanuel Cruz <emmanuel-cruz-dev@users.noreply.github.com>
2025-01-25 12:32:34 -0300

    estilos pulidos en Price component, a punto de instalar router y safe area

--- components/Price.jsx
+++ components/Price.jsx
@@ -1,14 +1,14 @@
 import { Text, View } from "react-native";
 
 export function Price({ price, maxPrice }) {
-  const getColors = () => {
+  const getColor = () => {
     const percent = (price / maxPrice) * 100;
-    if (percent > 600) return "bg-red-500 text-white";
-    if (percent > 200) return "bg-yellow-500 text-white";
-    return "bg-green-500 text-white";
+    if (percent > 600) return "bg-red-500";
+    if (percent > 200) return "bg-yellow-500";
+    return "bg-green-500";
   };
 
-  const className = getColors();
+  const className = getColor();
   return (
     <View
       className={`${className} w-1/3 rounded-full justify-center items-center`}
