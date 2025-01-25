42750aed0a7c3fea074b5431393288c7f44d220a
Emmanuel Cruz <emmanuel-cruz-dev@users.noreply.github.com>
2025-01-25 12:55:44 -0300

    componente Layout funcionando con estilos correctamente, preparando enrutado de todo el sitio

--- app/_layout.js
+++ app/_layout.js
@@ -2,8 +2,8 @@ import { View, Text } from "react-native";
 
 export default function Layout() {
   return (
-    <View className="flex-1 bg-black">
-      <Text className="text-white text-2xl">Hello!</Text>
+    <View className="flex-1 bg-black items-center justify-center">
+      <Text className="text-white text-2xl">Hallo!</Text>
     </View>
   );
 }
