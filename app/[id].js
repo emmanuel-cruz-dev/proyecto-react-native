e67260e3832964cb9693ebd18a24dd836320cc12
Emmanuel Cruz <emmanuel-cruz-dev@users.noreply.github.com>
2025-01-25 17:21:11 -0300

    agregando objeto a [id] preparando el renderizado de sus datos

--- app/[id].js
+++ app/[id].js
@@ -1,10 +1,20 @@
 import { Link, Stack } from "expo-router";
-import { Text, View } from "react-native";
+import { ActivityIndicator, Text, View } from "react-native";
 import { useLocalSearchParams } from "expo-router";
 import { Screen } from "../components/Screen";
+import { useEffect, useState } from "react";
+import { getProductDetails } from "../lib/metacritic";
+import { ScrollView } from "react-native";
 
 export default function Detail() {
   const { id } = useLocalSearchParams();
+  const [productInfo, setProductInfo] = useState(null);
+
+  useEffect(() => {
+    if (id) {
+      getProductDetails(id).then(setProductInfo);
+    }
+  }, [id]);
 
   return (
     <Screen>
@@ -18,12 +28,18 @@ export default function Detail() {
         }}
       />
       <View>
-        <Text className="text-white font-bold mb-8 text-2xl">
-          Detalle del producto {id}
-        </Text>
-        <Link href="/" className="text-blue-400">
-          Volver atrás
-        </Link>
+        {productInfo === null ? (
+          <ActivityIndicator size="large" color="#fff" />
+        ) : (
+          <ScrollView>
+            <Text className="text-white font-bold mb-8 text-2xl">
+              Detalle del producto {id}
+            </Text>
+            <Link href="/" className="text-blue-400">
+              Volver atrás
+            </Link>
+          </ScrollView>
+        )}
       </View>
     </Screen>
   );
