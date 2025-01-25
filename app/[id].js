b0e1b7f455d80764317bdd9e310fd22392a69ee1
Emmanuel Cruz <emmanuel-cruz-dev@users.noreply.github.com>
2025-01-25 17:38:39 -0300

    productdetails renderizado con detalles, imagen, titulo, precio y descripcion

--- app/[id].js
+++ app/[id].js
@@ -1,5 +1,5 @@
 import { Link, Stack } from "expo-router";
-import { ActivityIndicator, Text, View } from "react-native";
+import { ActivityIndicator, Text, View, Image } from "react-native";
 import { useLocalSearchParams } from "expo-router";
 import { Screen } from "../components/Screen";
 import { useEffect, useState } from "react";
@@ -32,9 +32,22 @@ export default function Detail() {
           <ActivityIndicator size="large" color="#fff" />
         ) : (
           <ScrollView>
-            <Text className="text-white font-bold mb-8 text-2xl">
-              Detalle del producto {id}
-            </Text>
+            <View className="justify-center">
+              <Image
+                className="mb-4 rounded mx-auto"
+                source={{ uri: productInfo.image }}
+                style={{ width: 240, height: 320, objectFit: "scale-down" }}
+              />
+              <Text className="text-white font-bold mb-8 text-2xl">
+                {productInfo.title}
+              </Text>
+              <Text className="text-white text-white/90 mb-4 text-2xl">
+                ${productInfo.price}
+              </Text>
+              <Text className="text-white text-white/90 mb-4">
+                {productInfo.description}
+              </Text>
+            </View>
             <Link href="/" className="text-blue-400">
               Volver atrás
             </Link>
