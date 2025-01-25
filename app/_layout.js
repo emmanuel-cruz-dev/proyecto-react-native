5964997308d198b480801025d4c413f9628c4ae4
Emmanuel Cruz <emmanuel-cruz-dev@users.noreply.github.com>
2025-01-25 12:52:35 -0300

    carpeta app agregada junto con sus archivos layout e index, paquetes de enrutamiento agregados y configuraciones cambiadas funcionando

---
+++ app/_layout.js
@@ -0,0 +1,9 @@
+import { View, Text } from "react-native";
+
+export default function Layout() {
+  return (
+    <View className="flex-1 bg-black">
+      <Text className="text-white text-2xl">Hello!</Text>
+    </View>
+  );
+}
